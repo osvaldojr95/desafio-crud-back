61f5e8774caddbbc4e1fb39fda416edc3965a5c5
osvaldojr95 <osvaldojunior95@gmail.com>
2022-12-22 00:22:01 -0300

    connected to mongoDB Atlas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,15 @@
 import express from "express";
+import db from "../config/db.js";
 
 const router = express.Router();
-router.get("/", (req, res) => {
-  res.send("OK");
+
+router.get("/", async (req, res) => {
+  try {
+    await db.collection("test").insertOne({ id: 1, name: "test" });
+    return res.sendStatus(201);
+  } catch (e) {
+    return res.status(500).send(e.message);
+  }
 });
+
 export default router;
